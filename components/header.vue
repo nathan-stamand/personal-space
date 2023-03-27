6171d277707fef409d8ca625e492418854955faa
nathan <nathansaint810@gmail.com>
2023-03-26 21:03:15 -0500

    Build out a few components for homepage

--- components/header.vue
+++ components/header.vue
@@ -1,11 +1,14 @@
 <template>
-  <header class="bg-slate-800">
-    <nav class="max-w-7xl mx-auto">
+  <header class="bg-slate-800 overflow-auto">
+    <button class="lg:hidden block w-16 h-16 bg-black/40 float-right" aria-haspopup="true" type="button" aria-controls="main-menu">
+      
+    </button>
+    <nav class="max-w-7xl mx-auto lg:block hidden" id="main-menu" role="menu">
       <ul class="flex flex-row justify-around items-center">
-        <li class="px-3 py-2"><a href='#about'>about</a></li>
-        <li class="px-3 py-2"><a href='#projects'>projects</a></li>
-        <li class="px-3 py-2"><a href='/resume'>resume</a></li>
-        <li class="px-3 py-2"><a href='#links'>links</a></li>
+        <li class="px-4 py-4" role="none"><a href='#about' role="menuitem">about</a></li>
+        <li class="px-4 py-4" role="none"><a href='#projects' role="menuitem">projects</a></li>
+        <li class="px-4 py-4" role="none"><a href='/resume' role="menuitem">resume</a></li>
+        <li class="px-4 py-4" role="none"><a href='#links' role="menuitem">links</a></li>
       </ul>
     </nav>
   </header>
---
+++ components/main.vue
@@ -0,0 +1,11 @@
+<template>
+  <main class="py-16 px-5 bg-body-bg bg-fixed bg-right-bottom lg:bg-center bg-cyan-900 bg-blend-multiply">
+    <slot />
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Main'
+}
+</script>
